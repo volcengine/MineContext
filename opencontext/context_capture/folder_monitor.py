fb55c15a2730bb7ce866659271ccae65bd15ba72
pjf1794643670@gmail.com <berry>
2025-12-04 01:28:11 +0800

    ref: 增加注释

--- opencontext/context_capture/folder_monitor.py
+++ opencontext/context_capture/folder_monitor.py
@@ -255,7 +255,7 @@ class FolderMonitorCapture(BaseCaptureComponent):
         try:
             # Find contexts by file_path
             contexts_dict = self._storage.get_all_processed_contexts(
-                context_types=[ContextType.KNOWLEDGE_CONTEXT],
+                context_types=[ContextType.KNOWLEDGE_CONTEXT],  # filter by context type
                 filter={
                     "knowledge_file_path": file_path,
                 },
